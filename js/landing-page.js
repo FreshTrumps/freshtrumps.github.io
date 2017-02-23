db587cf03d44e19951b4e1d965fcbd22a5b657aa
janstenpickle <jansen.chris@gmail.com>
2017-02-23 11:16:36 +0000

    Make better use of CDN

--- js/landing-page.js
+++ js/landing-page.js
@@ -29,3 +29,16 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+imgElements = document.getElementsByClassName('trump-img')
+
+for (var i = 0; i < imgElements.length; i++) {
+   imgElements[i].innerHTML = '<img src="https://trumps.lol/' + Math.floor(Math.random() * 30) + '.gif"></img>';
+}
+
+bgElements = document.getElementsByClassName('trump-bg')
+
+for (var i = 0; i < bgElements.length; i++) {
+   bgElements[i].style.background = 'url(https://trumps.lol/' + Math.floor(Math.random() * 30) + '.gif) no-repeat center center';
+   bgElements[i].style['background-size'] = 'cover';
+}
